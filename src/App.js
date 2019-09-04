9f651d4202c1f41249e771214621a6ff2ff8b964
ashfaqnisar <ashfaqnisar00@gmail.com>
2019-09-04 14:24:15 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from './firebase'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const FetchData = (props) => {
+    return (
+        <React.Fragment>
+            {props.Data.map(mappedData => <PrintData {...mappedData}/>)}
+        </React.Fragment>
+    )
+};
+const PrintData = (props) =>{
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Voltage: <a class="blue">{props.solar}V</a></p>
+          <p>Current: <a class="blue">{props.estimatedConsumption}mA</a></p>
+          <p>Power: <a class="blue">{props.consumption}mW</a></p>
+          <p>Shunt Voltage: <a class="blue">{props.efficiency}mV</a></p>
+        </header>
+      </div>
+    )
+}
+class App extends React.Component {
+  state = {
+    data : []
+  }
+  componentDidMount(){
+    const dataRef = firebase.database().ref('home_data');//This is for the comments
+        dataRef.on('value', (snapshot) => {
+            let AllTheData = snapshot.val();
+            let newState = [];
+
+            for (let item in AllTheData) {
+                newState.push({
+                    id: item,
+                    consumption: AllTheData[item].consumption,
+                    estimatedConsumption: AllTheData[item].estimatedConsumption,
+                    cost: AllTheData[item].cost,
+                    solar: AllTheData[item].solar,
+                    efficiency: AllTheData[item].efficiency
+                });
+            }
+            this.setState({
+                data: newState
+            })
+        });
+    }
+  constructor() {
+    super();
+  }
+  render(){
+    return(
+      <FetchData Data = {this.state.data}/>
+    )
+  }
 }
 
 export default App;
